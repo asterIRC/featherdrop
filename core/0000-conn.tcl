b9b37f425e1c664a04403b194ed7e2520b254043
Jaffa Cake <j4jackj@template.hostname>
2014-08-14 12:57:43 -0400

    thought i had tls verify off.

--- core/0000-conn.tcl
+++ core/0000-conn.tcl
@@ -1,8 +1,9 @@
 package require tls
 
 proc connect {addr port script} {
-	if {[string index $port 0] == "+"} { set port [string range $port 1 end] ; set comd ::tls::socket } {set comd socket}
-	set sck [$comd $addr $port]
+	set opts [list]
+	if {[string index $port 0] == "+"} { set port [string range $port 1 end] ; set comd ::tls::socket;set opts [list "-verify" 0]} {set comd socket}
+	set sck [$comd {*}$opts $addr $port]
 	fconfigure $sck -blocking 0 -buffering line
 	fileevent $sck readable [concat $script $sck]
 	return $sck
@@ -10,7 +11,7 @@ proc connect {addr port script} {
 
 proc listen {port type {sc ""}} {
 	if {$type=="all"} {set script ::pl::accept} {set script $sc}
-	if {[string index $port 0] == "+"} { set port [string range $port 1 end] ; set comd [list ::tls::socket -verify 0] } {set comd socket}
+	if {[string index $port 0] == "+"} { set port [string range $port 1 end] ; set comd [list ::tls::socket "-verify" 0] } {set comd socket}
 	set sck [{*}$comd -server $script -myaddr $::bindhost $port]
 }
 
